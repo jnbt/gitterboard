e6bfa9f8f3f89449591ded75b404489d18482e52
Jonas Thiel <jt@neopoly.de>
2016-06-27 11:06:16 +0200

    Add HTTPoison dependency

--- mix.exs
+++ mix.exs
@@ -17,7 +17,14 @@ defmodule GitterBoard.Mixfile do
   # Type `mix help compile.app` for more information.
   def application do
     [mod: {GitterBoard, []},
-     applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext]]
+     applications: [
+       :phoenix,
+       :phoenix_pubsub,
+       :phoenix_html,
+       :cowboy,
+       :logger,
+       :gettext,
+       :httpoison]]
   end
 
   # Specifies which paths to compile per environment.
@@ -33,6 +40,7 @@ defmodule GitterBoard.Mixfile do
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:httpoison, "~> 0.9"}]
   end
 end
